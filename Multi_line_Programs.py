88606ac5bf660fd178568179aa4b2f46b70565f2
mjmandelah07 <aramidemojisolagrace@gmail.com>
2021-07-15 23:08:59 +0100

    Modified files

---
+++ Multi_line_Programs.py
@@ -0,0 +1,42 @@
+#             MULTI LINE PROGRAMS
+#   So, in this lesson, you will write programs that print multiple lines.
+
+# Let's consider an example, the following code prints exactly three strings, each on a new line:
+
+print("I")
+print("know")
+print("Python")
+
+# The output is: I
+#                know
+#                Python
+
+
+# NOTE: The print function also allows  to print an empty line with no string specified:
+
+
+print("I")
+print()
+print("know")
+print()
+print("Python")
+
+# Here's the output:
+
+#                 I
+
+#               know
+
+#              Python
+
+
+# NOTE: However, skipping the line will have no effect:
+
+print("And")
+
+print("you?")
+
+# The output is:
+
+#     And
+#    you?
